8627ced38923a418ba1059c7c045700a5f13d292
jlli6 <jie.li6@uq.net.au>
2020-05-23 22:41:34 +1000

    commit bioutil-0.0.1

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name = 'bioutil',
+	version = '0.0.1',
+	author = 'Jie Li',
+	author_email = 'jeveylijie@gmail.com',
+	url = 'None',
+	description = 'bioutil lib for bio related bioinformatics operations.',
+	license = 'MIT3.0',
+	platform = 'Linux',
+	packages = ['bioutil']
+	)
