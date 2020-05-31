65133cc80f7fb5e3a2f597f5a85eae7844b3f9f8
jlli6 <jie.li6@uq.net.au>
2020-05-31 15:47:38 +1000

    add n50, gc and other features to sequtil

--- bioutil/sequtil.py
+++ bioutil/sequtil.py
@@ -36,7 +36,7 @@ class sequtil:
 			# use low-level parser to speed up when dealing with super large data
 			for t, seq in SimpleFastaParser(fasta_handle):
 				if len(seq) >= len_cutoff:
-					gc[t] = [cls._count_string_gc(seq), len(seq)]
+					gc[t] = [cls._count_string_gc(seq)/len(seq)*100., len(seq)]
 		return gc
 	
 	@classmethod
@@ -57,13 +57,13 @@ class sequtil:
 		# use low-level parser to speed up when dealing with large data
 		for t, seq, _ in FastqGeneralIterator(fastq_handle):
 			if len(seq) >= len_cutoff:
-				gc[t] = cls._count_string_gc(seq)
+				gc[t] = cls._count_string_gc(seq)/len(seq)*100.
 		fastq_handle.close()
 		return gc
 	
 	def _count_string_gc(s):
 		_total_gc = s.count('G') + s.count('C')+s.count('g') + s.count('c')
-		return round(_total_gc/len(s)*100., 2) # round to reduce float
+		return _total_gc
 
 	def read_fasta(fasta, length=False):
 		"""
@@ -109,21 +109,24 @@ class sequtil:
 		return seqs
 
 	@classmethod
-	def count_gap(cls, genome, ftype=str):
+	def count_gap(cls, genome, ftype=file):
 		"""
-		Count gaps in genome.
+		Count overall gaps number of a genome.
 
 		Parameters:
 		-----------
-		genome:str|file
-			Input genome FASTA format file or string of genome
-		ftype:str|file
-			input of genome is str or file.
+		genome:dict|file
+			Input genome FASTA format file or dict contains contigs.
+		ftype=dict|file
+			input of genome is dict or file. [file]
 
 		Returns:
 		--------
 		Return gap number
 		"""
+
+		if ftype is not file or is not dict:
+			sys.exit('Input ftype can only be dict or a FASTA file.')
 		
 		if genome is file:
 			genome = cls.read_fasta(genome)
@@ -132,6 +135,65 @@ class sequtil:
 			gap += len(re.findall('N+'), genome[i])
 		return gap
 
+
+	def n50(genome, ftype=file):
+		"""
+		Calculating n50 of a genome.
+
+		Parameters:
+		-----------
+		genome:list|file
+			A list of contig lengths or a FASTA formate file.
+		ftype=list|file
+			input genome is a list or a file [file].
+	
+		Returns:
+		--------
+		Return n50 of input genome.
+		"""
+
+		if ftype is not file or is not list:
+			sys.exit('Input type can only be list or a FASTA file.')
+		if genome is file:
+			genome = cls.read_fasta(genome, length=True).values()
+
+		genome.sort(reverse=True)
+		total_length = sum(genome)
+		sum_len = 0
+		for i in genome:
+			sum_len += i
+			if sum_len >= total_length*0.5:
+				return i
+	
+	@classmethod
+	def gc(genome, ftype=file):
+		"""
+		Calculate overall gc ratio of genome.
+		
+		Parameters:
+		-----------
+		genome:dict|file
+			input a dict of contigs or a FASTA file
+		ftype=dict|file
+			indicating input type of genome. [file]
+
+		Returns:
+		-------
+		Return genome size and gc ratio of input genome.
+		"""
+		
+		if ftype is not file or is not dict:
+			sys.exit('Input ftype can only be dict or a FASTA file.')
+		
+		if ftype is file:
+			genome = cls.read_fasta(genome)
+		gc = 0
+		total_length = 0
+		for i in genome:
+			total_length += len(genome[i])
+			gc += cls._count_string_gc(genome[i])
+			
+		return total_length, round(gc/total_length*100., 2)
 
 
 class seqmodify:
